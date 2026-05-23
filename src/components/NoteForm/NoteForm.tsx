edb1aa80be6abd59401de0c794e3fb6437c20dca
Mary1-com <vnnkvmrn76@gmail.com>
2026-05-23 21:03:39 +0300

    Commit before vercel - fix error mssage

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -6,12 +6,16 @@ import { createNote } from "../../services/noteService";
 
 interface NoteFormProps { onClose: () => void; }
 interface FormValues {
-    title: string; content: string; tag: | "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+    title: string;
+    content: string;
+    tag: | "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 }
 const initialValues: FormValues = { title: "", content: "", tag: "Todo", };
-const validationSchema = Yup.object({ title: Yup.string()    .min(3)    .max(50)    .required("Required"),
-    content: Yup.string().max(500), tag: Yup.string().oneOf([
-        "Todo", "Work", "Personal", "Meeting", "Shopping",]).required(),
+
+const validationSchema = Yup.object({
+    title: Yup.string().min(3, "Title must be at least 3 characters").max(50, "Title must be at most 50 haracters").required("Title is required"),
+    content: Yup.string().max(500, "Content must be at most 500 characters"),
+    tag: Yup.string().oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping",], "Invalid tag").required("Tag is required")
 });
 
 export default function NoteForm({ onClose, }: NoteFormProps) {
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import App from "./components/App/App";
 
@@ -11,5 +12,6 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
